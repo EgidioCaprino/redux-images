5308662e16e54c281b36e9133e4e481306cce859
Egidio Caprino <egidio.caprino@gmail.com>
2018-12-05 23:12:59 +0100

    fix: remove unnecessary exports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import reducer from './reducer';
-import receiveImagesUrls, { RECEIVE_IMAGES_URLS } from './receiveImagesUrls';
 import createFetchImagesUrlsAction from './createFetchImagesUrlsAction';
 import createPreloadImagesMiddleware from './createPreloadImagesMiddleware';
 
 export {
   reducer,
-  receiveImagesUrls,
-  RECEIVE_IMAGES_URLS,
   createFetchImagesUrlsAction,
   createPreloadImagesMiddleware,
 };
